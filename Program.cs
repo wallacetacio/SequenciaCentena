10ca60b8968759b938548b91d1ca8a17621a643f
Wallace Tácio <wallacett.dev@gmail.com>
2021-11-17 23:25:37 -0300

    Adiciona resolução ao exercício Sequência Centena ETEC 2021

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SequenciaCentena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nInicio;
+            int nFinal;
+
+            Console.WriteLine("100 primeiros números naturais não nulos.");
+
+            nInicio = 1;
+
+            nFinal = 100;
+
+            while(nInicio <= nFinal)
+            {
+                Console.Write($" {nInicio} ");
+                nInicio = nInicio + 1;
+            }
+        }
+    }
+}
